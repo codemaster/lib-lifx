c66289af80a596841171a5fdfe3fed7ceaedf5f7
Andrew Kane <codemaster@quantumraiders.com>
2015-08-29 15:47:29 -0700

    Added initial empty unit test project & dependency

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,20 @@
-function CommonConfig()
-	language "C++"
+newoption
+{
+	trigger = 'gtest',
+	description = 'root googletest source directory (defaults to ./googletest/googletest/)',
+	value = 'path',
+}
+
+local gtest_root = _OPTIONS['gtest'] or './googletest/googletest/'
+
+function UnitTestConfig()
+	includedirs { gtest_root .. '/include/' }
+	links { 'gtest' }
+end
+
+function LifxConfig()
+	includedirs { "./include/" }
+	files { "./include/**.h" }
 	
 	if os.get() ~= "windows" then
 		if os.get() == 'linux' then
@@ -9,6 +24,10 @@ function CommonConfig()
 	else
 		links { "ws2_32" }
 	end
+end
+
+function CommonConfig()
+	language "C++"
 	
 	configuration "Debug"
 		defines { "DEBUG" }
@@ -27,10 +46,10 @@ solution "lib-lifx"
 	project "lifx-cli"
 		kind "ConsoleApp"
 		targetname "lifx-cli"
-		files { "./include/**.h", "./src/cli/*.h", "./src/cli/*.cpp" }
+		files { "./src/cli/*.h", "./src/cli/*.cpp" }
 		dependson { "lib-lifx" }
 		links { "lib-lifx" }
-		includedirs { "./include/", "./src/cli/" }
+		includedirs { "./src/cli/" }
 
 		if os.get() == 'macosx' then
 			buildoptions
@@ -39,12 +58,39 @@ solution "lib-lifx"
 			}
 		end
 		
+		LifxConfig()
 		CommonConfig()
 		
 	project "lib-lifx"
 		kind "StaticLib"
 		targetname "lifx"
 		files { "./src/lib/*.cpp" }
-		includedirs { "./include/" }
+		
+		LifxConfig()
+		CommonConfig()
+
+	project "lifx-test"
+		kind "ConsoleApp"
+		files { "./test/*.cpp" }
+		links { "lib-lifx" }
+
+		LifxConfig()
+		UnitTestConfig()
+		CommonConfig()
+
+	-- gtest project adapted from Jim Garrison's (@garrison) premake4 script
+	-- http://jimgarrison.org/techblog/googletest-premake4.html
+	project "gtest"
+		language "C++"
+		kind "StaticLib"
+		includedirs { gtest_root, gtest_root .. '/include/' }
+		files { gtest_root .. '/src/gtest-all.cc', gtest_root .. '/src/gtest_main.cc' }
+
+		if os.get() ~= 'windows' then
+			defines { 'GTEST_HAS_PTHREAD=0' }
+			links { 'pthread' }
+		else
+			defines { 'GTEST_HAS_PTHREAD=0' }
+		end
 		
 		CommonConfig()
